91ba6c5308f1361110621b180d091548c8545e03
rtonata88 <rtonata@gmail.com>
2022-03-10 13:13:11 +0200

    Add getters and setters, is_of_age, and can_use_services methods

--- Person.rb
+++ Person.rb
@@ -5,4 +5,34 @@ class Person
         @name = name
         @age = age
     end
+
+    def get_id()
+        return @id
+    end
+
+     def get_name()
+        return @name
+    end
+
+    def get_age()
+        return @age
+    end
+
+    def set_name(name)
+        @name = name
+    end
+
+    def set_age(age)
+        @age = age
+    end
+
+    private
+
+    def is_of_age()
+        if @age >= 18
+            return true
+        else 
+            false
+        end
+    end
 end
