370f4e658ee44fc2b028c39fc99b016cc4a1b77b
rtonata88 <rtonata@gmail.com>
2022-03-10 15:02:21 +0200

    Add Teacher

---
+++ Teacher.rb
@@ -0,0 +1,11 @@
+require "./Person.rb"
+class Teacher < Person
+    initialize(specialization)
+        super()
+        @specialization = @specialization
+    end
+
+    def can_use_services?
+        return true
+    end
+end
