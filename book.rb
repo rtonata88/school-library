5a2a799aff80942a3c5261b4b640df8651c39064
rtonata88 <rtonata@gmail.com>
2022-03-11 15:35:54 +0200

    Add Book class

---
+++ book.rb
@@ -0,0 +1,11 @@
+#Defines the Book class blueprint
+class Book
+    def initialize(title, author)
+        @title = title
+        @author = author
+    end
+
+    attr_accessor :title, :author
+
+    
+end
