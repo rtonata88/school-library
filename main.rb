1ba5ef1e645b412f58062a2017048d12ba04f8e9
rtonata88 <rtonata@gmail.com>
2022-03-11 22:48:03 +0200

    Create method to add rentals and list them by people

--- main.rb
+++ main.rb
@@ -74,6 +74,86 @@ class Main
         start_menu
     end
 
+    def add_student
+        print "Student age: "
+        age = gets.chomp.to_i
+
+        print "Student name: "
+        name = gets.chomp
+
+        print "Has parent permission? [Y/N]"
+        parent_permission = gets.chomp
+        if parent_permission == 'Y'
+            parent_permission = true
+        else
+            parent_permission = false
+        end
+
+        @person << Student.new(age,name,parent_permission)
+        puts "Student created successfully!!!"
+
+        start_menu
+    end
+
+    def add_teacher
+        print "Age: "
+        age = gets.chomp.to_i
+
+        print "Name: "
+        name = gets.chomp
+
+        print "Specialization: "
+        specialization = gets.chomp
+    
+        @person << Teacher.new(age,name,specialization)
+        puts "Teacher created successfully!!!"
+
+        start_menu
+    end
+
+    #4. Create a book
+    def create_book
+        print "Title: "
+        title = gets.chomp
+        print "Author: "
+        author = gets.chomp
+        @book << Book.new(title, author)
+        puts "#{title} by #{author} created successfully!!!";
+        
+        #Display app main menu
+        start_menu
+    end
+
+    #5. Create a rental
+    def add_rental
+        puts "Select the person from the list below [input number]:"
+        @people.each_with_index {|person, index| puts "#{index}. #{person.name}" } end
+
+        select_person = gets.chomp.to_i
+        person = @people[selected_person]
+
+        puts "Select the book from the list below [input number]:"
+        @books.each_with_index {|book, index| puts "#{index}. #{book.title} by #{book.author}" } end
+
+        select_book = gets.chomp.to_i
+        book = @people[selected_book]
+
+        print 'Date: '
+        date = gets.chomp
+        @rentals << Rental.new(date, person, book)
+
+        puts "Rental successfully created !!!"
+    end
+
+    #6. List all rentals for a given person ID
+    def list_rentals_by_person
+        print 'Enter Person ID: '
+        person_id =  gets.chomp.to_i
+        @people.find { |person| person.id == person_id }.rentals.each do |rental|
+        puts rental
+        end
+    end
+
 end
 
 main = Main.new
