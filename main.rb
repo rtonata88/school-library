b973ed308f13a28f158f625bab03c790faa5283e
rtonata88 <rtonata@gmail.com>
2022-03-17 00:29:52 +0200

    Refactor main to show only starting of App

--- main.rb
+++ main.rb
@@ -4,21 +4,12 @@ require './student'
 require './teacher'
 require './rental'
 
-# Main class
-class Main
+def main
  app = App.new
  app.start
+end
 
-  def app_menu
-    puts 'Please choose an option by entering a number:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person ID'
-    puts '7. Exit'
-  end
+main
 
   # 1. List all books
   def list_all_books
