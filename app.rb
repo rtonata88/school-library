12d36718119002ca29e5143c79116dcee2dc2d2b
rtonata88 <rtonata@gmail.com>
2022-03-17 22:08:41 +0200

    Moved the all options that start the app to the main

--- app.rb
+++ app.rb
@@ -10,39 +10,6 @@ class App
     @rentals = []
   end
 
-  def start_menu
-    puts 'Please choose an option by entering a number:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person ID'
-    puts '7. Exit'
-  end
-
-  def start
-    start_menu
-    @option = gets.chomp
-
-    case @option.to_i
-    when 1 # List all books
-      list_all_books
-    when 2 # List all people
-      list_all_people
-    when 3 # Create a person
-      create_person
-    when 4 # Create a book
-      create_book
-    when 5 # Create a rental
-      add_rental
-    when 6 # List all rentals a given person ID
-      list_rentals_by_person
-    else # Exit
-      puts 'Goodbye :-)'
-    end
-  end
-
   # 1. List all books
   def list_all_books
     puts "\n \n"
@@ -52,9 +19,6 @@ class App
       @books.map { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     end
     puts "\n \n"
-
-    # Display app main menu
-    start
   end
 
   # 2. List all people
@@ -66,14 +30,10 @@ class App
       @people.map { |person| puts "[#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}" }
     end
     puts "\n \n"
-
-    # Display app main menu
-    start
   end
 
   # 3. Create a person
   def create_person
-    puts 'Do you want to create a (1) student or a (2) teacher [input the number]: '
     option = gets.chomp.to_i
 
     case option
@@ -84,9 +44,6 @@ class App
     else
       puts 'Invalid option selected, please select a valid option'
     end
-
-    # Display app main menu
-    start
   end
 
   def add_student
@@ -104,8 +61,6 @@ class App
     puts "\n \n"
     puts 'Student created successfully!!!'
     puts "\n \n"
-
-    start
   end
 
   def add_teacher
@@ -119,10 +74,10 @@ class App
     specialization = gets.chomp
 
     @people << Teacher.new(age, specialization, name)
+
     puts "\n \n"
     puts 'Teacher created successfully!!!'
     puts "\n \n"
-    start
   end
 
   # 4. Create a book
@@ -134,8 +89,6 @@ class App
     @books << Book.new(title, author)
     puts "#{title} by #{author} created successfully!!!"
 
-    # Display app main menu
-    start
   end
 
   # 5. Create a rental
@@ -164,9 +117,6 @@ class App
     new_rental = Rental.new(date, person, book)
     @rentals << new_rental
     puts "Rental successfully created !!! \n \n"
-
-    # Display app main menu
-    start
   end
 
   # 6. List all rentals for a given person ID
@@ -185,8 +135,5 @@ class App
     person = @rentals.select { |p| p.person.id == person_id }
     # rentals = person.rentals
     person.each { |rental| puts "Date: #{rental.date}, Book: #{rental.book.title}, Author: #{rental.book.author}" }
-
-    # Display app main menu
-    start
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,57 @@
 require './app'
 
-app = App.new
-app.start
+class Main 
+
+  def initialize
+    @app = App.new
+  end
+
+  def start_menu
+    puts 'Please choose an option by entering a number:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person ID'
+    puts '7. Exit'
+  end
+
+  def main_menu
+    start_menu
+    @option = gets.chomp
+
+    case @option.to_i
+    when 1 # List all books
+      @app.list_all_books
+
+      main_menu
+    when 2 # List all people
+      @app.list_all_people
+
+      main_menu
+    when 3 # Create a person
+      puts 'Do you want to create a (1) student or a (2) teacher [input the number]: '
+      @app.create_person
+
+      main_menu
+    when 4 # Create a book
+      @app.create_book
+
+      main_menu
+    when 5 # Create a rental
+      @app.add_rental
+      
+      main_menu
+    when 6 # List all rentals a given person ID
+      @app.list_rentals_by_person
+
+      main_menu
+    else # Exit
+      puts 'Goodbye :-)'
+    end
+  end
+end
+
+app = Main.new
+app.main_menu
